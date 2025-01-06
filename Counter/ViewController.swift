b9d5e5d8a9bdc1b7837e7aaa23f034f96cb022b5
Alex Agafonov <agafonov.alexander@gmail.com>
2025-01-07 00:28:49 +0300

    Создан проект

---
+++ Counter/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Counter
+//
+
+import UIKit
+import Foundation
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
