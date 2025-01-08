5b958317cfa13918bfe84fc4bd6e8c21da267ed4
Alex Agafonov <agafonov.alexander@gmail.com>
2025-01-08 23:44:24 +0300

    Внесение правок после ревью

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -6,16 +6,16 @@
 import UIKit
 import Foundation
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
     // создаем переменную для счетчика
     private var counter: Int = 0
     
     // аутлеты для интерфейсных элементов
-    @IBOutlet weak var counterLabel: UILabel!
-    @IBOutlet weak var logTextView: UITextView!
-    @IBOutlet weak var decrementButton: UIButton!
-    @IBOutlet weak var incrementButton: UIButton!
-    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet private weak var counterLabel: UILabel!
+    @IBOutlet private weak var logTextView: UITextView!
+    @IBOutlet private weak var decrementButton: UIButton!
+    @IBOutlet private weak var incrementButton: UIButton!
+    @IBOutlet private weak var resetButton: UIButton!
     
     // функция для форматирования даты
     private func getDate() -> String {
@@ -25,7 +25,7 @@ class ViewController: UIViewController {
         let formattedDate = dateFormatter.string(from: date)
         return formattedDate
     }
-   
+    
     // свойство для получения текущей даты и времени
     lazy private var formattedDate = getDate()
     
@@ -34,44 +34,38 @@ class ViewController: UIViewController {
         logTextView.text = "История изменений:\n" // пишем в textview начальное значение
         counterLabel.text = "\(counter)" // проставляем начальное значение счетчика
     }
-
     
-    @IBAction func decrementDidTap(_ sender: Any) {
+    // скролл после добавления строки, если она выходит за границы контроллера
+    private func scrollToRange() {
+        let range = NSMakeRange(logTextView.text.count - 1, 1)
+        logTextView.scrollRangeToVisible(range)
+    }
+    
+    
+    @IBAction private func decrementDidTap(_ sender: Any) {
         if counter > 0 { // проверяем не меньше ли нуля
             counter -= 1
             counterLabel.text = "Значение счётчика: \(counter)"
             logTextView.text += "\(formattedDate): " + "значение изменено на -1\n"
-            
-            // скролл после добавления строки, если она выходит за границы контроллера
-            let range = NSMakeRange(logTextView.text.count - 1, 1)
-            logTextView.scrollRangeToVisible(range)
+            scrollToRange()
         } else {
             logTextView.text += "\(formattedDate): попытка уменьшить значение счётчика ниже 0\n"
-            
-            // скролл после добавления строки, если она выходит за границы контроллера
-            let range = NSMakeRange(logTextView.text.count - 1, 1)
-            logTextView.scrollRangeToVisible(range)
+            scrollToRange()
         }
     }
- 
-    @IBAction func incrementDidTap(_ sender: Any) {
+    
+    @IBAction private func incrementDidTap(_ sender: Any) {
         counter += 1
         counterLabel.text = "Значение счётчика: \(counter)"
         logTextView.text += "\(formattedDate): " + "значение изменено на +1\n"
-                
-        // скролл после добавления строки, если она выходит за границы контроллера
-        let range = NSMakeRange(logTextView.text.count - 1, 1)
-        logTextView.scrollRangeToVisible(range)
+        scrollToRange()
     }
     
-    @IBAction func resetDidTap(_ sender: Any) {
+    @IBAction private func resetDidTap(_ sender: Any) {
         counter = 0
         counterLabel.text = "\(counter)"
         logTextView.text += "\(formattedDate): значение сброшено\n"
-        
-        // скролл после добавления строки, если она выходит за границы контроллера
-        let range = NSMakeRange(logTextView.text.count - 1, 1)
-        logTextView.scrollRangeToVisible(range)
+        scrollToRange( )
     }
     
 }
